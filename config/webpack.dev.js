3c85c9c7020501716f77f2f08d2b36f4d7f717b8
Stéphane MORI <stephane.mori@gmail.com>
2018-12-09 22:04:14 +0100

    chore(TOOLS): init tools to start to develop
    HTML, CSS and JS main files are now created empty.
    Webpack configuration allow to develop with hot reload. (npm run start)
    Webpack configuration allow to build the app in /dist directory. (npm run build:dev)
    Babel is configure to transpile for old browser.

---
+++ config/webpack.dev.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+
+module.exports = {
+    mode: "development",
+
+    entry: {
+        app: [
+            "babel-polyfill",
+            "./src/assets/js/app.js"
+        ]
+    },
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist")
+    },
+
+    plugins: [new CleanWebpackPlugin(
+        [ "dist" ],
+        {
+            root: path.resolve(__dirname, ".."),
+            verbose: true,
+            dry: false
+        }
+    )],
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: [{
+                loader: "babel-loader"
+            }],
+            exclude: /node_modules/
+        },{
+            test: /\.css$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }]
+        }, {
+            test: /\.html$/,
+            use: [{
+                loader: "file-loader",
+                options: {
+                    name: "[name].html"
+                }
+            }, {
+                loader: "extract-loader"
+            }, {
+                loader: "html-loader",
+                options: {
+                    attrs: ["img:src"]
+                }
+            }]
+        }, {
+            test: /\.(jpg|jpeg|png)$/,
+            use: {
+                loader: "file-loader",
+                options: {
+                    name: "images/[name].[ext]",
+                }
+            }
+        }]
+    },
+
+    devServer: {
+        contentBase: "dist",
+        overlay: true
+    },
+    devtool: "source-map"
+};
---
+++ src/assets/js/app.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+require("babel-runtime/regenerator");
+require("../css/main.css");
+require("../../index.html");
