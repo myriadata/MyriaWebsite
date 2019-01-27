7e2530e3e6a00078a395df2bb9bc1658820c2c30
Stéphane MORI <stephane.mori@gmail.com>
2019-01-27 20:31:58 +0100

    chore(TOOLS): exit continuous deployment with code error if deployment failed to have failed alert in circleci

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -12,6 +12,10 @@ var config = {
 }
 
 ftpDeploy.deploy(config, function(err) {
-    if (err) console.log(err)
-    else console.log('finished');
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    } else {
+        console.log('finished');
+    }
 });
