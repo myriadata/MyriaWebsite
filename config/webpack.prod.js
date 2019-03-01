a9eb901f7ec708d8cc2aa21d713673e85bd90656
Stéphane MORI <stephane.mori@gmail.com>
2019-03-02 00:59:47 +0100

    chore(TOOLS): minify html in production mode

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -36,7 +36,15 @@ module.exports = {
             }
         }),
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "./src/index.html",
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
         }),
         new CompressionPlugin({
           algorithm: 'gzip'
